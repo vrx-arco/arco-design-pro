e1d676109c9eefd5458d60506a3997a52e3eaf24
whitekite <1075790909@qq.com>
2022-07-21 09:17:31 +0800

    chore: 增加 changelog , release 命令

---
+++ changelog.config.ts
@@ -0,0 +1,14 @@
+export default {
+  types: {
+    feat: { title: '🚀 特性' },
+    perf: { title: '🔥 性能优化' },
+    fix: { title: '🩹 修复' },
+    refactor: { title: '💅 重构' },
+    examples: { title: '🏀 示例' },
+    docs: { title: '📖 文档' },
+    chore: { title: '🏡 框架' },
+    build: { title: '📦 打包' },
+    test: { title: '✅ 测试' },
+    BreakingChange: { title: '⚠️ 破坏性改动' },
+  },
+}
