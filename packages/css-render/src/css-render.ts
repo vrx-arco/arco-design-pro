1b63957c6a7ea2fb96594e938faa8cf942ac8eea
whitekite <1075790909@qq.com>
2022-07-22 10:06:16 +0800

    feat(css-render): `createCssRender` 返回 `bemClass` 方法帮助快速构建bem-class 名称

--- packages/css-render/src/css-render.ts
+++ packages/css-render/src/css-render.ts
@@ -1,10 +1,14 @@
 import { CssRender } from 'css-render'
 import { plugin as bem } from '@css-render/plugin-bem'
 
+export const blockPrefix = '.vrx-arco-'
+
 const { use: CssRenderUse, c } = CssRender()
+
 const bemPlugin = bem({
-  blockPrefix: '.vrx-arco-',
+  blockPrefix,
 })
+
 CssRenderUse(bemPlugin)
 const { cB, cM, cE } = bemPlugin
 export { c, cB, cM, cE }
--- packages/css-render/src/use.ts
+++ packages/css-render/src/use.ts
@@ -1,11 +1,18 @@
 import { CNode } from 'css-render'
 import { onBeforeMount } from 'vue'
 import { MountOption } from 'css-render/lib/types'
+import { blockPrefix } from './css-render'
 
 export const createCssRender = (css: CNode, options: MountOption = {}) => {
   return () => {
     onBeforeMount(() => {
       css.mount({ anchorMetaName: 'vrx-arco-style', ...options })
     })
+    const bemClass = (name: string) => {
+      return `${blockPrefix}${options.id}__${name}`
+    }
+    return {
+      bemClass,
+    }
   }
 }
