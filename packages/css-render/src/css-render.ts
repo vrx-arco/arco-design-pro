3acfbdb3601bf152f20b209f9eaebbb23a923b7d
whitekite <1075790909@qq.com>
2022-07-23 15:20:08 +0800

    feat(css-render): 优化 dts类型

--- packages/css-render/src/css-render.ts
+++ packages/css-render/src/css-render.ts
@@ -3,25 +3,13 @@ import { plugin as bem } from '@css-render/plugin-bem'
 import { MountOption } from 'css-render/lib/types'
 import { onBeforeMount } from 'vue'
 
-export interface CreateBemCssRender {
-  c: createCNode<CSelector>
-  cB: createCNode<string | CLazySelector<string>>
-  cM: createCNode<string | CLazySelector<string>>
-  cE: createCNode<string | CLazySelector<string>>
-  createCss: (
-    id: string,
-    cssChildren: CNodeChildren,
-    options?: Omit<MountOption, 'id'>
-  ) => () => {
-    bemClass: (name?: string) => string
-  }
-}
+export type { createCNode, CLazySelector, CNodeChildren, MountOption, CSelector }
 
 /**
  * 创建一个装载 bem插件的css-render
  * @param blockPrefix
  */
-export const createBemCssRender = (blockPrefix = 'vrx-arco'): CreateBemCssRender => {
+export const createBemCssRender = (blockPrefix = 'vrx-arco') => {
   const { use: CssRenderUse, c } = CssRender()
 
   const bemPlugin = bem({
