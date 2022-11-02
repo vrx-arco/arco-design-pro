7ef41a6a26498a9c568de85c0df491cb6db21b8e
whitekite <1075790909@qq.com>
2022-11-02 17:17:45 +0800

    feat(SearchBar): 修改自动 v-model 数据源为 computed 监听

--- packages/pro-components/src/SearchBar/SearchBarItem.tsx
+++ packages/pro-components/src/SearchBar/SearchBarItem.tsx
@@ -1,5 +1,5 @@
 import { FormGridItem } from '../FormGrid'
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { formGridItemProps } from '../FormGrid/props'
 import { injectSearchBar } from './context'
 import { bool, object, oneOfType, string } from 'vue-types'
@@ -14,34 +14,42 @@ export const SearchBarItem = defineComponent({
   setup: (props, { slots }) => {
     const { gridProps, model, autoUpdate: pAutoUpdate } = injectSearchBar()
 
-    const renderDefaultSlot = () => {
-      const defaultSlot = slots.default?.()
-      const { autoUpdate, field } = props
-      // 自动劫持默认插槽 模拟 `v-model:modelKey`
-      const isAutoUpdate = autoUpdate || pAutoUpdate.value
-      const firstDefaultSlotProps = defaultSlot?.[0]
-      if (!firstDefaultSlotProps) {
+    const modelValue = computed({
+      get: () => model.value[props.field],
+      set: (value) => {
+        model.value[props.field] = value
+      },
+    })
+
+    return () => {
+      const renderDefaultSlot = () => {
+        const defaultSlot = slots.default?.()
+        const { autoUpdate, field } = props
+        // 自动劫持默认插槽 模拟 `v-model:modelKey`
+        const isAutoUpdate = autoUpdate || pAutoUpdate.value
+        const firstDefaultSlotProps = defaultSlot?.[0]
+        if (!firstDefaultSlotProps) {
+          return defaultSlot
+        }
+        firstDefaultSlotProps.props ||= {}
+        if (isAutoUpdate && field) {
+          const modelKey: string = autoUpdate === true || !autoUpdate ? 'modelValue' : autoUpdate
+          firstDefaultSlotProps.props[modelKey] = modelValue.value
+          firstDefaultSlotProps.props[`onUpdate:${modelKey}`] = (value) => {
+            modelValue.value = value
+          }
+        }
         return defaultSlot
       }
-      firstDefaultSlotProps.props ||= {}
-      if (isAutoUpdate && field) {
-        const modelKey: string = autoUpdate === true || !autoUpdate ? 'modelValue' : autoUpdate
-        firstDefaultSlotProps.props[modelKey] = model.value[field]
-        firstDefaultSlotProps.props[`onUpdate:${modelKey}`] = (value) => {
-          model.value[field] = value
-        }
-      }
-      return defaultSlot
+      return (
+        <FormGridItem
+          {...props}
+          gridProps={props.gridProps || gridProps.value}
+          v-slots={{ label: slots.label, help: slots.help, extra: slots.extra }}
+        >
+          {renderDefaultSlot()}
+        </FormGridItem>
+      )
     }
-
-    return () => (
-      <FormGridItem
-        {...props}
-        gridProps={props.gridProps || gridProps.value}
-        v-slots={{ label: slots.label, help: slots.help, extra: slots.extra }}
-      >
-        {renderDefaultSlot()}
-      </FormGridItem>
-    )
   },
 })
