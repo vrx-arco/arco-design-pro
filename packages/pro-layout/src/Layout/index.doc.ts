044b482a7949911e2bfd2b31101a91cc2b5c9f6f
whitekite <1075790909@qq.com>
2023-03-14 09:58:03 +0800

    feat(ProLayout): update doc config

--- packages/pro-layout/src/Layout/index.doc.ts
+++ packages/pro-layout/src/Layout/index.doc.ts
@@ -17,6 +17,9 @@ export default defineGenDoc({
     default: {
       desc: '布局默认插槽-不使用该插槽时默认使用 RouterView',
     },
+    navContent: {
+      desc: '布局顶部中央空余',
+    },
   },
   arco: ['Drawer', 'Layout', 'Scrollbar', 'Spin', 'Menu', 'Space', 'Typography'],
 })
