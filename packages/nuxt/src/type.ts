d226143a6c83849724b97929b531c6126f000e99
whitekite <1075790909@qq.com>
2024-03-18 23:34:17 +0800

    feat(nuxt): 对 nuxt module 的入参选项类型，增加 默认值 jsdoc

--- packages/nuxt/src/type.ts
+++ packages/nuxt/src/type.ts
@@ -1,10 +1,12 @@
 export interface VrxArcoOption {
   /**
    * 引入的aro-design样式类型
+   * @default 'css'
    */
   importStyle?: 'css' | 'less'
   /**
    * 是否使用自动导入样式
+   * @default true
    */
   sideEffect?: boolean
   /**
@@ -13,14 +15,17 @@ export interface VrxArcoOption {
   theme?: string
   /**
    * 是否对内置的对 ssr 不友好的包进行自动转换配置
+   * @default true
    */
   transpile?: boolean
   /**
    * 是否对 @arco-deign/web-vue 提供自动导入支持
+   * @default false
    */
   arco?: boolean
   /**
    * 是否对 @arco-deign/web-vue/es/icon 提供自动导入支持
+   * @default false
    */
   resolveIcons?: boolean
 }
