694121f76d51d2aadadd3eeb73f1d644cf234d20
whitekite <1075790909@qq.com>
2025-01-07 01:29:31 +0800

    chore(helper): 迁移至 [vrx-arco/import-resolver](https://github.com/vrx-arco/import-resolver)

--- packages/helper/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './pro-components'
-export * from './pro-layout'
-export * as ProComponentsConstant from './pro-components/constant'
-export * as ProLayoutConstant from './pro-layout/constant'
